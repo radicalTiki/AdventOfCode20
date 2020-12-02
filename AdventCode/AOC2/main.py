30b3db2fa3bde0c6eec30e2841e34ac79f3fefd5
timekiller <gleason.matt@protonmail.com>
2020-12-02 14:46:17 -0500

    AOC2-2

--- AdventCode/AOC2/main.py
+++ AdventCode/AOC2/main.py
@@ -2,13 +2,25 @@ entries = open('input.txt').readlines()
 entries = [x.strip() for x in entries]
 
 
-def traverse(trav_entries):
+def traverse1(trav_entries):
     valid = 0
     for entry in entries:
         low, high = get_low_high(entry)
         character = get_character(entry)
         if low <= entry.split(":")[1].count(character) <= high:
             valid += 1
+    return valid
+
+
+def traverse2(trav_entries):
+    valid = 0
+    for entry in entries:
+        first, second = get_low_high(entry)
+        character = get_character(entry)
+        check = entry.split(":")[1]
+        if check[first] == character or check[second] == character:
+            if not check[first] == check[second]:
+                valid += 1
     return valid
 
 
@@ -24,5 +36,6 @@ def get_character(entry):
 
 
 if __name__ == '__main__':
-    answer = traverse(entries)
+    # answer = traverse1(entries)
+    answer = traverse2(entries)
     print("The answer = " + str(answer))
