3518ea039b181efd922065faf8a8304ec1b0c0d5
timekiller <gleason.matt@protonmail.com>
2020-12-01 19:06:15 -0500

    cleanup

--- AOC1/main.py
+++ AOC1/main.py
@@ -5,15 +5,15 @@ entries = open('input.txt').readlines()
 answer = 0
 entries = [x.strip() for x in entries]
 
-for entry in entries:
-    for entry2 in entries[1:]:
-        for entry3 in entries[2:]:
-            if int(entry) + int(entry2) + int(entry3) == 2020:
-                answer = int(entry) * int(entry2) * int(entry3)
-                break
-        if answer != -0:
-            break
-    if answer != 0:
-        break
 
-print("The answer = " + str(answer))
+def traverse(trav_entries):
+    for entry in trav_entries:
+        for entry2 in trav_entries[1:]:
+            for entry3 in trav_entries[2:]:
+                if int(entry) + int(entry2) + int(entry3) == 2020:
+                    return int(entry) * int(entry2) * int(entry3)
+
+
+if __name__ == '__main__':
+    answer = traverse(entries)
+    print("The answer = " + str(answer))
